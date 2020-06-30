41aa05111b825567bacae75b2ca9362ece9a708c
Stu Sklinar <stuart.sklinar@k3>
2020-06-30 15:05:57 +0100

    Code

--- UnitTestDemo.Services/CalculatorService.cs
+++ UnitTestDemo.Services/CalculatorService.cs
@@ -11,11 +11,13 @@ namespace UnitTestDemo.Services
         Multiply,
         Modulus
     }
+
     public interface ICalculatorService
     {
         int Add(int num1, int num2);
         int Operation(int num1, int num2, CalculatorOperation calculatorOperation);
     }
+
 
     public class CalculatorService : ICalculatorService
     {
@@ -24,9 +26,31 @@ namespace UnitTestDemo.Services
             return num1 + num2;
         }
 
+        private int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
         public int Operation(int num1, int num2, CalculatorOperation calculatorOperation)
         {
-            throw new NotImplementedException();
+
+            //Add if for num1/2
+
+            switch (calculatorOperation)
+            {
+                case CalculatorOperation.Add:
+                    return Add(num1, num2);
+                case CalculatorOperation.Subtract:
+                    return Subtract(num1, num2);
+                case CalculatorOperation.Divide:
+                    return Add(num1, num2); // I know these are wrong.
+                case CalculatorOperation.Multiply:
+                    return Add(num1, num2);
+                case CalculatorOperation.Modulus:
+                    return Add(num1, num2);
+                default:
+                    return 0;
+            }
         }
     }
 }
